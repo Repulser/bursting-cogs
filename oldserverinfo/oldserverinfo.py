3b7037d6956a23878b74459daf14cd18c50a58ec
Repulser <thetechproject2014@gmail.com>
2016-11-24 20:30:18 +0200

    Create oldserverinfo.py

---
+++ oldserverinfo/oldserverinfo.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+class serverinfo:
+    @commands.command(pass_context=True, no_pm=True)
+    async def serverinfo(self, ctx):
+        """Shows server's informations"""
+        server = ctx.message.server
+        online = str(len([m.status for m in server.members if str(m.status) == "online" or str(m.status) == "idle"]))
+        total_users = str(len(server.members))
+        text_channels = len([x for x in server.channels if str(x.type) == "text"])
+        voice_channels = len(server.channels) - text_channels
+
+        data = "```python\n"
+        data += "Name: {}\n".format(server.name)
+        data += "ID: {}\n".format(server.id)
+        data += "Region: {}\n".format(server.region)
+        data += "Users: {}/{}\n".format(online, total_users)
+        data += "Text channels: {}\n".format(text_channels)
+        data += "Voice channels: {}\n".format(voice_channels)
+        data += "Roles: {}\n".format(len(server.roles))
+        passed = (ctx.message.timestamp - server.created_at).days
+        data += "Created: {} ({} days ago)\n".format(server.created_at, passed)
+        data += "Owner: {}\n".format(server.owner)
+        if server.icon_url != "":
+            data += "Icon:"
+            data += "```"
+            data += server.icon_url
+        else:
+            data += "```"
+        await self.bot.say(data)
+def setup(bot):
+    n = serverinfo(bot)
+    bot.add_cog(n)
