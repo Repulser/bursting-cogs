14f3bde6f3a22e5a10c20b248cd0b84f2f5ffeeb
Repulser <thetechproject2014@gmail.com>
2016-11-25 10:02:33 +0200

    Create pogoservers.py

---
+++ pogoservers/pogoservers.py
@@ -0,0 +1,38 @@
+import requests
+import discord
+from discord.ext import commands
+from random import choice, randint
+from bs4 import BeautifulSoup
+class pogo:
+    """pogo"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context = True)
+    async def pogoservers(self, ctx):
+        url = "http://cmmcd.com/PokemonTrainerClub/"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
+        links = soup.find_all('font')
+        for item in links:
+            level = item.text
+        url = "http://cmmcd.com/PokemonGo/"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
+        links = soup.find_all('font')
+        for item in links:
+            level2 = item.text
+        color = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+        color = int(color, 16)
+        data = discord.Embed(description=level, colour=discord.Colour(value=color))
+        data.add_field(name="Pokémon Go Server Status", value=level2)
+        data.set_footer(text="Pokémon Go Servers Status")
+        data.set_author(name="PTC Login status")
+        await self.bot.say(embed=data)
+
+
+def setup(bot):
+    n = pogo(bot)
+    bot.add_cog(n)
+
