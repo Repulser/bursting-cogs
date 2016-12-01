a70c079218a2691d848243e03d7aae02a087339e
Repulser <thetechproject2014@gmail.com>
2016-12-01 19:20:29 +0200

    Create pingtime.py

---
+++ pingtime/pingtime.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from random import choice, randint
+class pingtime:
+    """Ping, with time"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def ping(self, ctx):
+        """Pong."""
+        t1 = time.perf_counter()
+        await self.bot.send_typing(ctx.message.channel)
+        t2 = time.perf_counter()
+        thedata = ("**Pong.**\nTime: " + str(round((t2-t1)*1000)) + "ms")
+        color = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+        color = int(color, 16)
+        data = discord.Embed(description=thedata, colour=discord.Colour(value=color))
+        
+        await self.bot.say(embed=data)
+     
+
+def setup(bot):
+    n = pingtime(bot)
+    bot.add_cog(n)
