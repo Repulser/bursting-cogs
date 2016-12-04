fb1fa0fd076823b2dfa807d193257d5c575cc5d7
Repulser <thetechproject2014@gmail.com>
2016-12-04 15:39:24 +0200

    Updated with new error messages

--- discorole/discorole.py
+++ discorole/discorole.py
@@ -12,28 +12,35 @@ class disco:
         self.bot = bot
     @checks.mod_or_permissions(moderator=True)
     @commands.command(pass_context = True, no_pm=True)
-    async def discorole(self, ctx, times : int, role: discord.Role, interval : float):
+    async def discorole(self, ctx, times:int, interval:float, *, role):
+        roleObj = discord.utils.find(lambda r: r.name == role, ctx.message.server.roles)
+        if not roleObj:
+            await self.bot.say("`{}` is not a valid role".format(role))
+            return
         if interval < 1:
             interval = 1.5
         time = 0
         while time < times:
             colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
             colour = int(colour, 16)
-            await self.bot.edit_role(ctx.message.server, role, colour=discord.Colour(value=colour))
+            await self.bot.edit_role(ctx.message.server, roleObj, colour=discord.Colour(value=colour))
             time = time + 1
             await asyncio.sleep(interval)
     
     @checks.admin_or_permissions(administrator=True)
     @commands.command(pass_context = True, no_pm=True)
-    async def discoroleforever(self, ctx, role: discord.Role, interval : float):
+    async def discoroleforever(self, ctx, interval:float, *, role):
+        roleObj = discord.utils.find(lambda r: r.name == role, ctx.message.server.roles)
+        if not roleObj:
+            await self.bot.say("`{}` is not a valid role".format(role))
+            return
         if interval < 1:
             interval = 1.5
         while True:
             colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
             colour = int(colour, 16)
-            await self.bot.edit_role(ctx.message.server, role, colour=discord.Colour(value=colour))
+            await self.bot.edit_role(ctx.message.server, roleObj, colour=discord.Colour(value=colour))
             await asyncio.sleep(interval)
-
 
 def setup(bot):
     n = disco(bot)
