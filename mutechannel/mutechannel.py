79933eab5430ec1f8385fe046c053ae3815724d4
Repulser <thetechproject2014@gmail.com>
2016-12-04 18:56:53 +0200

    added unmute

--- mutechannel/mutechannel.py
+++ mutechannel/mutechannel.py
@@ -20,6 +20,16 @@ class channelmute:
         await asyncio.sleep(5)
         await self.bot.delete_message(todelete)
 
+    @checks.admin_or_permissions(manage_channels=True)
+    @commands.command(pass_context = True)
+    async def unmutechannel(self, ctx):
+        perms = discord.PermissionOverwrite()
+        perms.send_messages = True
+        await self.bot.edit_channel_permissions(ctx.message.channel, ctx.message.server.default_role ,perms)
+        todelete = await self.bot.say("Done :+1:")
+        await asyncio.sleep(5)
+        await self.bot.delete_message(todelete)
+
 def setup(bot):
     n = channelmute(bot)
     bot.add_cog(n)
