7a3c091bb1be269ce870a641a178ab6835fdf261
Repulser <thetechproject2014@gmail.com>
2016-12-05 20:48:37 +0200

    Create talk.py

---
+++ talk/talk.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import cleverbot
+class talk:
+    """Talk with the bot!"""
+    def __init__(self, bot):
+        self.bot = bot
+		
+    @commands.command(pass_context = True)
+    async def talk(self, ctx):
+        cleverbot_client = cleverbot.Cleverbot()
+        await self.bot.say(":fire: type exit to quit")
+        while True:
+            question = await self.bot.wait_for_message(author=ctx.message.author, channel=ctx.message.channel, timeout = 60)
+            try:
+                if question.content == "exit":
+                    await self.bot.say(":+1:")
+                    break
+                await self.bot.send_typing(ctx.message.channel)
+                answer = cleverbot_client.ask(question.content)
+                await self.bot.say(answer)
+            except AttributeError:
+                await self.bot.say("Ok then, well talk later")
+                break
+
+def setup(bot):
+    n = talk(bot)
+    bot.add_cog(n)
