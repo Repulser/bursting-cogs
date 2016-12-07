76ca4098633813d5e581ba7c60cfbca38ea2e9df
Repulser <thetechproject2014@gmail.com>
2016-12-07 18:22:26 +0200

    Create countdown.py

---
+++ countdown/countdown.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+import asyncio
+class countdown:
+    """Countdown timer!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context = True)
+    async def countdown(self, ctx, seconds, title):
+        counter = 0
+        try:
+            secondint = int(seconds)
+            message = await self.bot.say("```css" + "\n" + "[" + title +"]" + "\nTimer: " + seconds + "```")
+            while True:
+                secondint = secondint - 1
+                if secondint == -10:
+                    await self.bot.edit_message(message, new_content=("```Ended!```"))
+                    break
+                await self.bot.edit_message(message, new_content=("```css" + "\n" + "[" + title + "]" + "\nTimer: {0}```".format(secondint)))
+                await asyncio.sleep(1)
+        except ValueError:
+            await self.bot.say("Must be a number!")
+
+
+def setup(bot):
+    n = countdown(bot)
+    bot.add_cog(n)
