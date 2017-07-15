9f905a612a50b7ae6b97abc2d36f059019212cfb
Repulser <thetechproject2014@gmail.com>
2017-07-16 00:26:44 +0300

    Fix pogoservers

--- pogoservers/pogoservers.py
+++ pogoservers/pogoservers.py
@@ -24,16 +24,10 @@ class pogo:
         links = soup.find_all('font')
         for item in links:
             level2 = item.text
-        resp = await self.session.get(url, headers={'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3'})
-        x = await resp.text()
-        decoded = x.decode("utf-8")[2:]
         color = ''.join([choice('0123456789ABCDEF') for x in range(6)])
         color = int(color, 16)
         data = discord.Embed(description=level, colour=discord.Colour(value=color))
         data.add_field(name="Pokémon Go Server Status", value=level2)
-        listd = list(decoded)
-        if listd[0] != "t":
-            data.add_field(name="Api version", value=decoded)
         data.set_author(name="PTC Login status")
         await self.bot.say(embed=data)
 
